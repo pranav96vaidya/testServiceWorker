5363d018aa19441eaa23c1afa920007fe35e6f6b
Pranav <pranav@newput.com>
2019-08-13 14:19:53 +0530

    remove service worker from app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,6 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ServiceWorkerModule } from '@angular/service-worker';
-import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -12,8 +10,7 @@ import { environment } from '../environments/environment';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    ServiceWorkerModule.register('./ngsw-worker.js', { enabled: environment.production })
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
